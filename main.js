ae5faa1a4fc3b7755aa444cd1c0af236c384d1fe
ZestyPoptart <poppyy.poptart@gmail.com>
2024-09-01 03:30:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+/* Getting and storing name */
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Hiiii, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Hiiii, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
+
+/* Changing image */
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src")
+    if (mySrc === "Images/Sonic_miku.jpg") {
+        myImage.setAttribute("src", "Images/Miku.webp");
+    } else {
+        myImage.setAttribute("src", "Images/Sonic_miku.jpg")
+    }
+};
