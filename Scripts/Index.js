d4e1ae089f57448ccaa4aedffff0551840201ed4
poppy <alexamezquita67@gmail.com>
2024-09-08 03:02:27 -0700

    enter

--- Scripts/Index.js
+++ Scripts/Index.js
@@ -8,7 +8,7 @@ btn.onclick = function () {
 
 
 /* Getting and storing name */
-const myButton = document.querySelector("button");
+const myButton = document.getElementsByClassName(".Change_user");
 const myHeading = document.querySelector("h1");
 
 function setUserName() {
